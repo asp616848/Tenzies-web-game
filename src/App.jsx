242c101cd8120e7c6c68adad0aaefdfaefa35a64
Abhijeet Shashwat <asp616848@gmail.com>
2025-03-24 01:57:40 +0530

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,8 @@
-import React from "react";
+ import Die from "./Die"
 
 export default function App() {
     return (
-        <main style={{ 
-            backgroundColor: "#F5F5F5", 
-            borderRadius: "20px", 
-            padding: "20px", 
-            color: "#333", 
-            fontFamily: "'Inter', sans-serif",
-            maxWidth: "600px",
-            margin: "50px auto",
-            textAlign: "center"
-        }}>
-            <h1>Welcome to Tenzies!</h1>
-            <p>Start your game here.</p>
-        </main>
-    );
+    <main>
+
+    </main>)
 }
--- src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client"; // Use "react-dom" if React version < 18
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root")); // Ensure there's a div with id="root" in your HTML
-root.render(<App />);
