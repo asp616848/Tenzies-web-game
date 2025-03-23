95d89d8d4eb3835b26573054f1a2e8cef8f17b14
Abhijeet Shashwat <asp616848@gmail.com>
2025-03-24 01:33:35 +0530

    a

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function App() {
+    return (
+        <main style={{ 
+            backgroundColor: "#F5F5F5", 
+            borderRadius: "20px", 
+            padding: "20px", 
+            color: "#333", 
+            fontFamily: "'Inter', sans-serif",
+            maxWidth: "600px",
+            margin: "50px auto",
+            textAlign: "center"
+        }}>
+            <h1>Welcome to Tenzies!</h1>
+            <p>Start your game here.</p>
+        </main>
+    );
+}
