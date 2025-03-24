31725e9b73954024d605c10c5c08bde7fbfebca1
Abhijeet Shashwat <asp616848@gmail.com>
2025-03-24 12:47:46 +0530

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
- import Die from "./Die"
+import Die from "./Die"
+import React from "react";
 
 export default function App() {
+    const[ diceValues, setDice] = React.useState(new Array(10).fill(0).map(() =>
+        Math.ceil(Math.random() * 6)
+    ));
+    let a = 1;
     return (
     <main>
-        
-    </main>)
+        <div className="dice-container">
+            {diceValues.map((val, ind) => {
+                console.log(val, ind);  // Log values correctly
+                return <Die key={ind} value={val} />;  // Return JSX properly
+            })}
+        </div>
+        <button name="Roll">
+            Roll
+        </button>
+    </main>
+    )
 }
